33c777e484da7109b59e43688b38036ac0526f40
Theboiboi8 <kamentanchev@gmail.com>
2024-04-03 13:55:40 +0300

    Release 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -7,33 +7,105 @@ use std::ops::{Add, Sub};
 #[must_use]
 /// A grid-based two-dimensional representation of a mathematical vector.
 pub struct Vector2D {
-    /// Defines the origin point of the vector on a grid centered around (0, 0).
-    pub origin: (f64, f64),
+    /// Defines the origin point of the vector.
+    pub origin: Point2D,
     /// The end of the vector in relation to its origin.
-    pub target: (f64, f64)
+    ///
+    /// While this is a [`Point2D`] type,
+    /// much like `origin`, it represents a relative value
+    /// rather than an absolute one.
+    pub target: Point2D
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+#[must_use]
+/// A structure that represents a point in two-dimensional space.
+/// 
+/// The grid is centered around (0, 0).
+pub struct Point2D {
+    /// The x coordinate of the [`Point2D`]
+    pub x: f64,
+    /// The y coordinate of the [`Point2D`]
+    pub y: f64,
+}
+
+impl Point2D {
+    /// Constructs a new [`Point2D`] at coordinates (0, 0).
+    pub fn zero() -> Self {
+        Point2D {
+            x: 0.0,
+            y: 0.0,
+        }
+    }
+
+    /// Constructs a new [`Point2D`] at the given coordinates.
+    pub fn new(x: f64, y: f64) -> Self {
+        Point2D {
+            x,
+            y,
+        }
+    }
+
+    /// Sets the `x` coordinate of [`self`]
+    pub fn set_x(mut self, x: f64) -> Self {
+        self.x = x;
+        self
+    }
+
+    /// Sets the `y` coordinate of [`self`]
+    pub fn set_y(mut self, y: f64) -> Self {
+        self.y = y;
+        self
+    }
+
+    /// Shifts the left-hand-side [`Point2D`] by the right-hand-side tuple of types
+    /// which implement [`Into<f64>`].
+    pub fn shift(mut self, shift: (impl Into<f64>, impl Into<f64>)) -> Self {
+        self.x += shift.0.into();
+        self.y += shift.1.into();
+        self
+    }
+
+    /// Returns the distance between [`self`] and the given [`Point2D`].
+    pub fn get_distance(&self, rhs: Point2D) -> f64 {
+        f64::sqrt(
+            (self.x - rhs.x).powf(2.0)
+                +
+                (self.y - rhs.y).powf(2.0)
+        )
+    }
+    
+    /// Constructs a new [`Vector2D`] with an `origin` [`Point2D`]([`self`]) and an absolute
+    /// `target` [`Point2D`]
+    pub fn to_vector2d(&self, target: &Point2D) -> Vector2D {
+        Vector2D {
+            origin: *self,
+            target: *target
+        }
+    }
 }
 
 impl Vector2D {
     /// A zero [`Vector2D`] with both `origin` and `target` at (0, 0).
     pub fn zero() -> Self {
         Vector2D {
-            origin: (0.0, 0.0),
-            target: (0.0, 0.0),
+            origin: Point2D { x: 0.0, y: 0.0},
+            target: Point2D { x: 0.0, y: 0.0},
         }
     }
 
     /// A [`Vector2D`] with a `target` of (0, 0).
     ///
     /// Also known as a zero-length vector
-    pub fn null(origin: (f64, f64)) -> Self {
+    pub fn null(origin: Point2D) -> Self {
         Vector2D {
             origin,
-            target: (0.0, 0.0),
+            target: Point2D { x: 0.0, y: 0.0},
         }
     }
 
     /// Constructs a [`Vector2D`] from the provided `origin` and `target`.
-    pub fn new(origin: (f64, f64), target: (f64, f64)) -> Self {
+    pub fn new(origin: Point2D, target: Point2D) -> Self {
         Vector2D {
             origin,
             target,
@@ -47,21 +119,24 @@ impl Vector2D {
     /// meaning that `Vector2D::with_absolute_target((1.0,0.0),(2.0,1.0))`
     /// constructs a vector with an `origin` of (1.0, 0.0) but a `target`
     /// of (1.0, 1.0) as opposed to a `target` of (2.0, 1.0).
-    pub fn with_absolute_target(origin: (f64, f64), target: (f64, f64)) -> Self {
+    pub fn with_absolute_target(origin: Point2D, target: Point2D) -> Self {
         Vector2D {
             origin,
-            target: (target.0 - origin.0, target.1 - origin.1),
+            target: Point2D {
+                x: target.x - origin.x,
+                y: target.y - origin.y
+            },
         }
     }
 
     /// Sets the `origin` of [`self`]
-    pub fn set_origin(mut self, origin: (f64, f64)) -> Self {
+    pub fn set_origin(mut self, origin: Point2D) -> Self {
         self.origin = origin;
         self
     }
 
     /// Sets the `target` of [`self`]
-    pub fn set_target(mut self, target: (f64, f64)) -> Self {
+    pub fn set_target(mut self, target: Point2D) -> Self {
         self.target = target;
         self
     }
@@ -76,32 +151,39 @@ impl Vector2D {
     /// 
     /// So far, I have only encountered this issue with this function,
     /// but if you encounter any others during use, please report them in the issue tracker.
-    pub fn set_target_absolute(mut self, target: (f64, f64)) -> Self {
-        self.target = (
-            ((target.0 - self.origin.0) * 1_000_000.0).round() / 1_000_000.0,
-            ((target.1 - self.origin.1) * 1_000_000.0).round() / 1_000_000.0,
-        );
+    pub fn set_target_absolute(mut self, target: Point2D) -> Self {
+        self.target = Point2D {
+            x: ((target.x - self.origin.x) * 1_000_000.0).round() / 1_000_000.0,
+            y: ((target.y - self.origin.y) * 1_000_000.0).round() / 1_000_000.0,
+        };
         self
     }
 
     /// Shifts the left-hand-side [`Vector2D`] by the right-hand-side tuple of types
-    /// which implement [`Into<f32>`].
+    /// which implement [`Into<f64>`].
     ///
     /// The resulting [`Vector2D`] has the same `target` value, as it's relative to its `origin`,
     /// but a shifted `origin`, which results in an overall shift of the [`Vector2D`].
     pub fn shift(mut self, shift: (impl Into<f64>, impl Into<f64>)) -> Self {
-        self.origin = (self.origin.0 + shift.0.into(), self.origin.1 + shift.1.into());
+        self.origin = Point2D {
+            x: self.origin.x + shift.0.into(),
+            y: self.origin.y + shift.1.into()
+        };
         self
     }
 
     /// Returns the magnitude of the given [`Vector2D`].
     pub fn get_magnitude(&self) -> f64 {
-        f64::sqrt((self.origin.0 - self.target.0).powf(2.0) + (self.origin.1 - self.target.1).powf(2.0))
+        f64::sqrt(
+            (self.origin.x - self.target.x).powf(2.0)
+                +
+                (self.origin.y - self.target.y).powf(2.0)
+        )
     }
     
     /// Returns the dot product of the given [`Vector2D`]s.
     pub fn dot_product(&self, vector: Vector2D) -> f64 {
-        self.target.0 * vector.target.0 + self.target.1 * vector.target.1
+        self.target.x * vector.target.x + self.target.y * vector.target.y
     }
 
     /// Returns the angle between the given [`Vector2D`]s in radians.
@@ -117,11 +199,11 @@ impl Display for Vector2D {
     /// 
     /// Example:
     /// ```rust
-    /// use simple_2d_vector::Vector2D;
+    /// use simple_2d_vector::{Vector2D, Point2D};
     /// 
     /// let vector = Vector2D::new(
-    ///     (2.2, 1.1),
-    ///     (1.1, 2.2)
+    ///     Point2D { x: 2.2, y: 1.1 },
+    ///     Point2D { x: 1.1, y: 2.2 },
     /// );
     /// 
     /// assert_eq!(format!("{vector}"), "(2.2,1.1)[1.1,2.2]");
@@ -130,10 +212,10 @@ impl Display for Vector2D {
         write!(
             f,
             "({},{})[{},{}]",
-            self.origin.0,
-            self.origin.1,
-            self.target.0,
-            self.target.1,
+            self.origin.x,
+            self.origin.y,
+            self.target.x,
+            self.target.y,
         )
     }
 }
@@ -145,7 +227,10 @@ impl Add for Vector2D {
     fn add(self, rhs: Self) -> Self::Output {
         Vector2D {
             origin: self.origin,
-            target: (rhs.target.0 + self.target.0, rhs.target.1 + self.target.1),
+            target: Point2D {
+                x: rhs.target.x + self.target.x,
+                y: rhs.target.y + self.target.y
+            },
         }
     }
 }
@@ -157,7 +242,45 @@ impl Sub for Vector2D {
     fn sub(self, rhs: Self) -> Self::Output {
         Vector2D {
             origin: self.origin,
-            target: (self.target.0 - rhs.target.0, self.target.1 - rhs.target.1),
+            target: Point2D {
+                x: self.target.x - rhs.target.x,
+                y: self.target.y - rhs.target.y
+            },
+        }
+    }
+}
+
+impl<T: Into<f64>, E: Into<f64>> From<(T, E)> for Point2D {
+    fn from(value: (T, E)) -> Self {
+        let value: (f64, f64) = (value.0.into(), value.1.into());
+
+        Point2D {
+            x: value.0,
+            y: value.1,
+        }
+    }
+}
+
+impl Add for Point2D {
+    type Output = Self;
+
+    /// Performs a mathematical addition of two [`Point2D`]s.
+    fn add(self, rhs: Self) -> Self::Output {
+        Point2D {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl Sub for Point2D {
+    type Output = Self;
+
+    /// Performs a mathematical subtraction of two [`Point2D`]s.
+    fn sub(self, rhs: Self) -> Self::Output {
+        Point2D {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
         }
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,15 +1,15 @@
 use std::ops::{Add, Sub};
-use crate::Vector2D;
+use crate::{Vector2D, Point2D};
 
 #[test]
 fn new_vector() {
 	let new_vector = Vector2D::new(
-		(0.0, 0.0),
-		(0.0, 0.0)
+		(0.0, 0.0).into(),
+		(0.0, 0.0).into()
 	);
 	let vector = Vector2D {
-		origin: (0.0, 0.0),
-		target: (0.0, 0.0)
+		origin: (0.0, 0.0).into(),
+		target: (0.0, 0.0).into()
 	};
 	assert_eq!(vector, new_vector);
 }
@@ -17,160 +17,160 @@ fn new_vector() {
 fn zero_vector() {
 	let zero_vector = Vector2D::zero();
 	let vector = Vector2D::new(
-		(0.0, 0.0),
-		(0.0, 0.0)
+		(0.0, 0.0).into(),
+		(0.0, 0.0).into()
 	);
 	assert_eq!(vector, zero_vector);
 }
 
 #[test]
 fn null_vector() {
-	let null_vector = Vector2D::null((5.0, 5.0));
+	let null_vector = Vector2D::null((5.0, 5.0).into());
 	let vector = Vector2D::new(
-		(5.0, 5.0),
-		(0.0, 0.0)
+		(5.0, 5.0).into(),
+		(0.0, 0.0).into()
 	);
 	assert_eq!(vector, null_vector);
 }
 #[test]
 fn with_absolute_target_vector() {
 	let vector = Vector2D::new(
-		(5.0, 5.0),
-		(5.0, 5.0)
+		(5.0, 5.0).into(),
+		(5.0, 5.0).into()
 	);
 	let absolute_target_vector = Vector2D::with_absolute_target(
-		(5.0, 5.0),
-		(10.0, 10.0)
+		(5.0, 5.0).into(),
+		(10.0, 10.0).into()
 	);
 	assert_eq!(vector, absolute_target_vector);
 }
 #[test]
 fn set_origin() {
-	let set_origin_vector = Vector2D::zero().set_origin((1.1, 2.2));
+	let set_origin_vector = Vector2D::zero().set_origin((1.1, 2.2).into());
 	let vector = Vector2D::new(
-		(1.1, 2.2),
-		(0.0, 0.0)
+		(1.1, 2.2).into(),
+		(0.0, 0.0).into()
 	);
 	assert_eq!(vector, set_origin_vector);
 }
 #[test]
 fn set_target() {
-	let set_target_vector = Vector2D::zero().set_target((1.1, 2.2));
+	let set_target_vector = Vector2D::zero().set_target((1.1, 2.2).into());
 	let vector = Vector2D::new(
-		(0.0, 0.0),
-		(1.1, 2.2)
+		(0.0, 0.0).into(),
+		(1.1, 2.2).into()
 	);
 	assert_eq!(vector, set_target_vector);
 }
 #[test]
 fn set_target_absolute() {
-	let set_target_vector = Vector2D::null((1.0, 5.0)).set_target_absolute((1.1, 2.2));
+	let set_target_vector = Vector2D::null((1.0, 5.0).into()).set_target_absolute((1.1, 2.2).into());
 	let vector = Vector2D::new(
-		(1.0, 5.0),
-		(0.1, -2.8)
+		(1.0, 5.0).into(),
+		(0.1, -2.8).into()
 	);
 	assert_eq!(vector, set_target_vector);
 }
 #[test]
 fn display_vector() {
 	let vector = Vector2D::new(
-		(2.2, 1.1),
-		(1.1, 2.2)
+		(2.2, 1.1).into(),
+		(1.1, 2.2).into()
 	);
 	assert_eq!(format!("{vector}"), "(2.2,1.1)[1.1,2.2]");
 }
 #[test]
 fn add_vectors() {
 	let vector1 = Vector2D::new(
-		(10.0, 10.0),
-		(10.0, 5.0)
+		(10.0, 10.0).into(),
+		(10.0, 5.0).into()
 	);
 	let vector2 = Vector2D::new(
-		(10.0, 10.0),
-		(5.0, 10.0)
+		(10.0, 10.0).into(),
+		(5.0, 10.0).into()
 	);
 	let result_vector = Vector2D::new(
-		(10.0, 10.0),
-		(15.0, 15.0)
+		(10.0, 10.0).into(),
+		(15.0, 15.0).into()
 	);
 	assert_eq!(vector1 + vector2, result_vector);
 }
 #[test]
 fn addition() {
 	let vector1 = Vector2D::new(
-		(10.0, 10.0),
-		(10.0, 5.0)
+		(10.0, 10.0).into(),
+		(10.0, 5.0).into()
 	);
 	let vector2 = Vector2D::new(
-		(10.0, 10.0),
-		(5.0, 10.0)
+		(10.0, 10.0).into(),
+		(5.0, 10.0).into()
 	);
 	assert_eq!(vector1 + vector2, vector1.add(vector2));
 }
 #[test]
 fn sub_vectors() {
 	let vector1 = Vector2D::new(
-		(10.0, 10.0),
-		(10.0, 5.0)
+		(10.0, 10.0).into(),
+		(10.0, 5.0).into()
 	);
 	let vector2 = Vector2D::new(
-		(10.0, 10.0),
-		(5.0, 10.0)
+		(10.0, 10.0).into(),
+		(5.0, 10.0).into()
 	);
 	let result_vector = Vector2D::new(
-		(10.0, 10.0),
-		(5.0, -5.0)
+		(10.0, 10.0).into(),
+		(5.0, -5.0).into()
 	);
 	assert_eq!(vector1 - vector2, result_vector);
 }
 #[test]
 fn subtraction() {
 	let vector1 = Vector2D::new(
-		(10.0, 10.0),
-		(10.0, 5.0)
+		(10.0, 10.0).into(),
+		(10.0, 5.0).into()
 	);
 	let vector2 = Vector2D::new(
-		(10.0, 10.0),
-		(5.0, 10.0)
+		(10.0, 10.0).into(),
+		(5.0, 10.0).into()
 	);
 	assert_eq!(vector1 - vector2, vector1.sub(vector2));
 }
 #[test]
 fn shift_positive() {
 	let vector = Vector2D::new(
-		(10.0, 10.0),
-		(10.0, 5.0)
+		(10.0, 10.0).into(),
+		(10.0, 5.0).into()
 	);
 	let tuple = (2u16, 1.25);
 	let result_vector = Vector2D::new(
-		(12.0, 11.25),
-		(10.0, 5.0)
+		(12.0, 11.25).into(),
+		(10.0, 5.0).into()
 	);
 	assert_eq!(vector.shift(tuple), result_vector);
 }
 #[test]
 fn shift_negative() {
 	let vector = Vector2D::new(
-		(10.0, 10.0),
-		(10.0, 5.0)
+		(10.0, 10.0).into(),
+		(10.0, 5.0).into()
 	);
 	let tuple = (-2i16, -1.25);
 	let result_vector = Vector2D::new(
-		(8.0, 8.75),
-		(10.0, 5.0)
+		(8.0, 8.75).into(),
+		(10.0, 5.0).into()
 	);
 	assert_eq!(vector.shift(tuple), result_vector);
 }
 #[test]
 fn shift_mixed() {
 	let vector = Vector2D::new(
-		(10.0, 10.0),
-		(10.0, 5.0)
+		(10.0, 10.0).into(),
+		(10.0, 5.0).into()
 	);
 	let tuple = (-2i16, 1.25);
 	let result_vector = Vector2D::new(
-		(8.0, 11.25),
-		(10.0, 5.0)
+		(8.0, 11.25).into(),
+		(10.0, 5.0).into()
 	);
 	assert_eq!(vector.shift(tuple), result_vector);
 }
@@ -178,8 +178,8 @@ fn shift_mixed() {
 fn get_magnitude() {
 	let error_margin = f64::EPSILON;
 	let vector = Vector2D::new(
-		(0.0, 0.0),
-		(3.0, 4.0)
+		(0.0, 0.0).into(),
+		(3.0, 4.0).into()
 	);
 	let magnitude = 5.0;
 	assert!((vector.get_magnitude() - magnitude).abs() < error_margin);
@@ -188,27 +188,78 @@ fn get_magnitude() {
 fn get_dot_product() {
 	let error_margin = f64::EPSILON;
 	let vector1 = Vector2D::new(
-		(0.0, 0.0),
-		(3.0, 4.0)
+		(0.0, 0.0).into(),
+		(3.0, 4.0).into()
 	);
 	let vector2 = Vector2D::new(
-		(0.0, 0.0),
-		(5.0, 7.0)
+		(0.0, 0.0).into(),
+		(5.0, 7.0).into()
 	);
 	let dot_product = 43.0;
 	assert!((vector1.dot_product(vector2) - dot_product).abs() < error_margin);
 }
 #[test]
 fn get_angle_between() {
-	let error_margin = f64::EPSILON;
 	let vector1 = Vector2D::new(
-		(0.0, 0.0),
-		(3.0, 4.0)
+		(0.0, 0.0).into(),
+		(3.0, 4.0).into()
 	);
 	let vector2 = Vector2D::new(
-		(0.0, 0.0),
-		(5.0, 7.0)
+		(0.0, 0.0).into(),
+		(5.0, 7.0).into()
 	);
 	let angle_between = 0.023_251_623;
-	assert_eq!((vector1.get_angle_between(vector2)* 1_000_000_000.0).round() / 1_000_000_000.0, angle_between);
+	assert_eq!(
+		(vector1.get_angle_between(vector2)* 1_000_000_000.0).round() / 1_000_000_000.0,
+		angle_between
+	);
+}
+#[test]
+fn new_point() {
+	let point = Point2D {
+		x: 0.0,
+		y: 1.1,
+	};
+	let new_point = Point2D::new(
+		0.0, 1.1
+	);
+	assert_eq!(point, new_point);
+}
+#[test]
+fn zero_point() {
+	let point = Point2D::new(
+		0.0,
+		0.0
+	);
+	let zero_point = Point2D::zero();
+	assert_eq!(point, zero_point);
+}
+#[test]
+fn point_distance() {
+	let point1 = Point2D::new(
+		0.0,
+		0.0
+	);
+	let point2 = Point2D::new(
+		1.0,
+		0.0
+	);
+	let distance = 1.0;
+	assert_eq!(point1.get_distance(point2), distance);
+}
+#[test]
+fn to_vector2d() {
+	let point1 = Point2D::new(
+		0.0,
+		0.0
+	);
+	let point2 = Point2D::new(
+		1.0,
+		0.0
+	);
+	let vector = Vector2D::new(
+		(0.0,0.0).into(),
+		(1.0,0.0).into(),
+	);
+	assert_eq!(point1.to_vector2d(&point2), vector);
 }
